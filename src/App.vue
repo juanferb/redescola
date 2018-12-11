ca27151d289dd904e37d6cc1b41ea195124ad974
juanferb <juanferb@users.noreply.github.com>
2018-12-11 02:38:39 +0100

    Añadir menú y sección para contenido dinámico

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <v-app>
+    <v-toolbar>
+      <v-toolbar-title>Redescola</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>
+          <v-icon left>view_list</v-icon>
+          VER CURSOS
+        </v-btn>
+      </v-toolbar-items>
+      
     
+    </v-toolbar><!-- Componente toolbar de Vuetify: Contenido fijo -->
+    <main> <!-- Contenido dinámico -->
+
+    </main>
   </v-app>
 </template>
 
