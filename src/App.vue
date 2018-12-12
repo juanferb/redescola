d3f03f1740add982804c4612ad22fd6ff890a667
juanferb <juanferb@users.noreply.github.com>
2018-12-12 01:43:53 +0100

    Eliminar parámetro router (redundante)

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <!-- Componente drawer de Vuetify: Contenido fijo, menú lateral -->
     <v-navigation-drawer fixed v-model="sideNav" temporary>
       <v-list>
-        <v-list-tile v-for="item in menuItems" :key="item.title" router :to="item.link">
+        <v-list-tile v-for="item in menuItems" :key="item.title" :to="item.link">
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
@@ -20,7 +20,7 @@
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat v-for="item in menuItems" :key="item.title" router :to="item.link">
+        <v-btn flat v-for="item in menuItems" :key="item.title" :to="item.link">
           <v-icon left dark>{{ item.icon }}</v-icon>
           {{ item.title }}
         </v-btn>
