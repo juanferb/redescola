37d6083be6fd8443b14e0d6352be71f48125859f
juanferb <juanferb@users.noreply.github.com>
2018-12-12 01:40:58 +0100

    Corregir errores eslint

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
 <template>
   <v-app>
     <!-- Componente drawer de Vuetify: Contenido fijo, menú lateral -->
-    <v-navigation-drawer
-      fixed 
-      v-model="sideNav"
-      temporary>
+    <v-navigation-drawer fixed v-model="sideNav" temporary>
       <v-list>
-        <v-list-tile  
-          v-for="item in menuItems" 
-          :key="item.title"
-          router
-          :to="item.link">
+        <v-list-tile v-for="item in menuItems" :key="item.title" router :to="item.link">
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
@@ -21,21 +14,13 @@
 
     <!-- Componente toolbar de Vuetify: Contenido fijo -->
     <v-toolbar dark class="primary">
-      <v-toolbar-side-icon 
-        @click="sideNav = !sideNav"
-        class="hidden-sm-and-up">
-      </v-toolbar-side-icon>
+      <v-toolbar-side-icon @click="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
       <v-toolbar-title>
-        <router-link to="/" tag="span" style="cursor: pointer">Redescola</router-link> 
+        <router-link to="/" tag="span" style="cursor: pointer">Redescola</router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn 
-          flat 
-          v-for="item in menuItems" 
-          :key="item.title"
-          router
-          :to="item.link">
+        <v-btn flat v-for="item in menuItems" :key="item.title" router :to="item.link">
           <v-icon left dark>{{ item.icon }}</v-icon>
           {{ item.title }}
         </v-btn>
@@ -43,7 +28,7 @@
     </v-toolbar>
 
     <!-- Contenido dinámico -->
-    <main> 
+    <main>
       <router-view></router-view>
     </main>
   </v-app>
@@ -55,11 +40,11 @@ export default {
     return {
       sideNav: false,
       menuItems: [
-        { icon: 'view_list', title: 'Ver Cursos', link:'/cursos'},
-        { icon: 'room', title: 'Crear Curso', link:'/curso/crear'},
-        { icon: 'person', title: 'Perfil', link:'/perfil'},
-        { icon: 'face', title: 'Rexistro', link:'/rexistro'},
-        { icon: 'lock_open', title: 'Login', link:'/login'},
+        { icon: 'view_list', title: 'Ver Cursos', link: '/cursos' },
+        { icon: 'room', title: 'Crear Curso', link: '/curso/crear' },
+        { icon: 'person', title: 'Perfil', link: '/perfil' },
+        { icon: 'face', title: 'Rexistro', link: '/rexistro' },
+        { icon: 'lock_open', title: 'Login', link: '/login' }
       ]
     }
   },
--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,78 +1,69 @@
 <template>
-    <v-container>
+  <v-container>
+    <!-- Botones superiores -->
+    <v-layout row wrap>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/cursos" class="info">Ver Cursos</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/curso/crear" class="info">Crear Curso</v-btn>
+      </v-flex>
+    </v-layout>
 
-        <!-- Botones superiores -->
-        <v-layout row wrap>
-            <v-flex xs12 sm6 class="text-xs-center text-sm-right">
-                <v-btn large router to="/cursos" class="info">Ver Cursos</v-btn>
-            </v-flex>
-            <v-flex xs12 sm6 class="text-xs-center text-sm-left">
-                <v-btn large router to="/curso/crear" class="info">Crear Curso</v-btn>
-            </v-flex>
-        </v-layout>
+    <!-- Carousel -->
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item v-for="curso in cursos" :src="curso.imageUrl" :key="curso.id">
+            <div class="title">{{ curso.title }}</div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
 
-        <!-- Carousel -->
-        <v-layout row wrap class="mt-2">
-            <v-flex xs12>
-                <v-carousel>
-                    <v-carousel-item 
-                        v-for="curso in cursos"
-                        :src="curso.imageUrl"
-                        :key="curso.id">
-                        <div class="title">
-                            {{ curso.title }}
-                        </div>    
-                    </v-carousel-item>
-                </v-carousel>
-            </v-flex>
-        </v-layout>
-
-        <!-- Párrafo inferior -->
-        <v-layout row wrap class="mt-2">
-            <v-flex xs12 class="text-xs-center text-sm-right">
-                <p style="text-align:center">Disfruta dos nosos cursos!</p>
-            </v-flex>
-        </v-layout>
-    </v-container>
+    <!-- Párrafo inferior -->
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12 class="text-xs-center text-sm-right">
+        <p style="text-align:center">Disfruta dos nosos cursos!</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-    export default {
-        data () {
-            return {
-                cursos: [
-                    { 
-                        imageUrl: 'https://static1.ideal.es/www/multimedia/201811/01/media/cortadas/movil-kFzD-U601417053636zZC-624x385@Ideal.jpg', 
-                        id: '1', 
-                        title: 'Curso móviles' 
-                    },
-                    { 
-                        imageUrl: 'https://cdn.pixabay.com/photo/2017/04/26/16/06/mobile-2262928_960_720.jpg', 
-                        id: '2', 
-                        title: 'Curso móviles 2' 
-                    },
-                    { 
-                        imageUrl: 'https://cdn.pixabay.com/photo/2017/04/26/16/06/mobile-2262928_960_720.jpg', 
-                        id: '3', 
-                        title: 'Curso móviles 3' 
-                    },
-                ]
-            }
+export default {
+  data () {
+    return {
+      cursos: [
+        {
+          imageUrl:
+            'https://static1.ideal.es/www/multimedia/201811/01/media/cortadas/movil-kFzD-U601417053636zZC-624x385@Ideal.jpg',
+          id: '1',
+          title: 'Curso móviles'
+        },
+        {
+          imageUrl:
+            'https://cdn.pixabay.com/photo/2017/04/26/16/06/mobile-2262928_960_720.jpg',
+          id: '2',
+          title: 'Curso móviles 2'
         }
+      ]
     }
+  }
+}
 </script>
 
 <style scoped>
-    .title {
-        position: absolute;
-        bottom: 50px;
-        background-color: rgba(0, 0, 0, 0.5);
-        color: white;
-        font-size: 2em;
-        padding: 15px;
-        justify-content: center;
-        left: 50%;
-        transform: translateX(-50%);
-    }
+.title {
+  position: absolute;
+  bottom: 50px;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  font-size: 2em;
+  padding: 15px;
+  justify-content: center;
+  left: 50%;
+  transform: translateX(-50%);
+}
 </style>
 
