2dcf7083cabdd27318ec97b73063f84a8bc35159
juanferb <juanferb@users.noreply.github.com>
2018-12-13 01:51:43 +0100

    Crear formulario de registro y añadir validaciones

--- src/components/Usuario/Rexistro.vue
+++ src/components/Usuario/Rexistro.vue
@@ -1,5 +1,78 @@
 <template>
-    <div>
-        <p>Páxina de rexistro</p>
-    </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card-text>
+          <v-container>
+            <form @submit.prevent="onSignup">
+              <v-layout row>
+                <v-flex xs12>
+                  <v-text-field
+                    name="email"
+                    label="Mail"
+                    id="email"
+                    v-model="email"
+                    type="email"
+                    required>
+                  </v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12>
+                  <v-text-field
+                    name="password"
+                    label="Contrasinal"
+                    id="password"
+                    v-model="password"
+                    type="password"
+                    required>
+                  </v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12>
+                  <v-text-field
+                    name="confirmPassword"
+                    label="Confirmar contrasinal"
+                    id="confirmPassword"
+                    v-model="confirmPassword"
+                    type="password"
+                    :rules="[compararPasswords]">
+                  </v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12>
+                  <v-btn type="submit">Rexistrarse</v-btn>
+                </v-flex>
+              </v-layout>
+            </form>
+          </v-container>
+        </v-card-text>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  },
+  computed: {
+    compararPasswords () {
+      return this.password !== this.confirmPassword ? 'As contrasinais non coinciden' : ''
+    }
+  },
+  methods: {
+    onSignup () {
+      // Vuex
+    }
+  }
+}
+</script>
+
